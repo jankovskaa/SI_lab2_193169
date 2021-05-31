1eca4dbd1964ab9308ab24c49800dc27829b9555
jankovskaa <anastasija.jankovska007@gamil.com>
2021-05-31 21:29:41 +0200

    First commitcd

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -15,56 +15,30 @@ class SILab2Test {
     @Test
     void everyBranchStatement() {
 
-        lista.add(new Time(1,0,1));
+        lista.add(new Time(1,1,1));
         rez.add(3661);
         assertEquals(rez, SILab2.function(lista));
 
-        lista.add(new Time(25,0,0));
+        lista.add(new Time(24,0,0));
         rez.add(86400);
         assertEquals(rez, SILab2.function(lista));
 
-        lista.add(new Time(-4,11,13));
+        lista.add(new Time(-7,11,17));
         mess = assertThrows(RuntimeException.class, () -> SILab2.function(lista));
         assertTrue(mess.getMessage().contains("The hours are smaller than the minimum"));
 
         List<Time> lista1 = new LinkedList<>();
-        lista1.add(new Time(27,11,13));
+        lista1.add(new Time(32,13,10));
         mess = assertThrows(RuntimeException.class, () -> SILab2.function(lista1));
-        assertTrue(mess.getMessage().contains("The hours are grater than the maximum"));
-
+        assertTrue(mess.getMessage().contains("The hours are greater than the maximum"));
         List<Time> lista2 = new LinkedList<>();
-        lista2.add(new Time(15,-3,10));
-        mess = assertThrows(RuntimeException.class, () -> SILab2.function(lista2));
-        assertTrue(mess.getMessage().contains("The minutes are not valid!"));
-
-        List<Time> lista3 = new LinkedList<>();
-        lista3.add(new Time(14,17,-10));
-        mess = assertThrows(RuntimeException.class, () -> SILab2.function(lista3));
-        assertTrue(mess.getMessage().contains("The seconds are not valid"));
-
-        List<Time> lista4 = new LinkedList<>();
-        List<Integer> r = new ArrayList<>();
-        assertEquals(r, SILab2.function(lista4));
-
-    }
-
-    @Test
-    void multipleCondition(){
-        //if (hr < 0 || hr > 24)
-        lista.add(new Time(-4,11,13));
-        mess = assertThrows(RuntimeException.class, () -> SILab2.function(lista));
-        assertTrue(mess.getMessage().contains("The hours are smaller than the minimum"));
-
-        List<Time> lista1 = new LinkedList<>();
-        lista1.add(new Time(27,11,13));
-        mess = assertThrows(RuntimeException.class, () -> SILab2.function(lista1));
-        assertTrue(mess.getMessage().contains("The hours are grater than the maximum"));
-
-        List<Time> lista2 = new LinkedList<>();
-        lista2.add(new Time(12,-19,25));
+        lista2.add(new Time(10,-8,9));
         mess = assertThrows(RuntimeException.class, () -> SILab2.function(lista2));
         assertTrue(mess.getMessage().contains("The minutes are not valid!"));
 
     }
 
 }
+
+
+
